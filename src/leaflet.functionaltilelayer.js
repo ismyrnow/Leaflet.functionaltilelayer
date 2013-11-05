791fab5ece2f68605fb014761620c8e39e2415a3
Ishmael Smyrnow <ismyrnow@gmail.com>
2013-11-04 22:03:23 -0500

    Support maxNativeZoom
    - See issue
    https://github.com/ismyrnow/Leaflet.functionaltilelayer/issues/3
    - and issue https://github.com/Leaflet/Leaflet/pull/1802

--- src/leaflet.functionaltilelayer.js
+++ src/leaflet.functionaltilelayer.js
@@ -10,35 +10,36 @@ L.TileLayer.Functional = L.TileLayer.extend({
   getTileUrl: function (tilePoint) {
     // Note: bbox code untested; pulled from TileLayer.WMS
     var map = this._map,
-        crs = map.options.crs,
-        tileSize = this.options.tileSize,
-        zoom = this._map.getZoom(),
-        nwPoint = tilePoint.multiplyBy(tileSize),
-        sePoint = nwPoint.add(new L.Point(tileSize, tileSize)),
-        nw = crs.project(map.unproject(nwPoint, zoom)),
-        se = crs.project(map.unproject(sePoint, zoom)),
-        bbox = [nw.x, se.y, se.x, nw.y].join(','),
-        view = {
-          bbox: bbox,
-          width: tileSize,
-          height: tileSize,
-          zoom: zoom,
-          tile: {
-            row: tilePoint.y,
-            column: tilePoint.x
-          },
-          subdomain: this._getSubdomain(tilePoint)
-        };
+      crs = map.options.crs,
+      tileSize = this.options.tileSize,
+      zoom = tilePoint.z,
+      nwPoint = tilePoint.multiplyBy(tileSize),
+      sePoint = nwPoint.add(new L.Point(tileSize, tileSize)),
+      nw = crs.project(map.unproject(nwPoint, zoom)),
+      se = crs.project(map.unproject(sePoint, zoom)),
+      bbox = [nw.x, se.y, se.x, nw.y].join(','),
+      view = {
+        bbox: bbox,
+        width: tileSize,
+        height: tileSize,
+        zoom: zoom,
+        tile: {
+          row: tilePoint.y,
+          column: tilePoint.x
+        },
+        subdomain: this._getSubdomain(tilePoint)
+      };
 
     return this._tileFunction(view);
   },
 
   _loadTile: function (tile, tilePoint) {
-    var tileUrl = this.getTileUrl(tilePoint);
-
     tile._layer = this;
     tile.onload = this._tileOnLoad;
     tile.onerror = this._tileOnError;
+
+    this._adjustTilePoint(tilePoint);
+    var tileUrl = this.getTileUrl(tilePoint);
 
     if (typeof tileUrl === "string") {
       tile.src = tileUrl;
